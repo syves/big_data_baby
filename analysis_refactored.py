eb8062200b773a2c90cd713f240e924f3309245b
syves <7shakrahs@gmail.com>
2015-02-19 00:11:46 -0800

    major refactoring, on transformers and metrics, still need to fix other metrics and write to csv

--- analysis_refactored.py
+++ analysis_refactored.py
@@ -6,86 +6,83 @@ import os
 #2. and outputs combined to a researcher_data.csv?
 
 #transform all researcher data into a iterable collection
-#now all researchers are in one file, each row is a company
-'''
-def helper_transform_researchers_data():
-    files = os.listdir('data') #=> ['file.csv', 'file2.csv']
-   # print files
-    companies = []
-    #or should I do all the files in analyst_takehome
-#and for file in files.endswith('.csv')
-    for file in files:
-        #print file
-        #print os.path.abspath(file)
-        with open(os.path.abspath(file)) as csvfile:
-            #=> '/Users/syves/Documents/analyst_takehome/is_closed_classification_Andres.csv'
-    #should be '/Users/syves/Documents/analyst_takehome/data/is_closed_classification_Andres.csv'
-            readable = csv.reader(csvfile)
-            for row in readable:
-                companies.append(row)
-        #transform array into dictionary
-        return {id:{"id": id, "name":name, "is_closed": is_closed == 'Y'}
-                        for id, name, address, city, state, is_closed in companies}
-print helper_transform_researchers_data()
-'''
 
-def helper_transform_sources():
-    files = os.listdir('analyst_takehome')
-    print files #=> ['hidden_info.json']
-    print os.path.isdir('sources')#=> TRUE
+def transform_researcher_data(filename):
+    with open(filename) as csvfile:
+        return {id: {"id": id, "name":name, "is_closed": is_closed == 'Y'}
+                for id, name, address, city, state, is_closed in csv.reader(csvfile)}
 
-    for file in files:
-        if file.endswith('.json'):
-            with open(os.path.abspath(file)) as jsonfile:
-            #transform json object into dictionary of dictionaries, with is_close as boolean, and id as key
-                return {key: {"id": key,
-                              "source": value["source"],
-                              "is_closed": value["source_label_is_closed"] == "Y"}
-                            for key, value in json.load(jsonfile).items()}
-    #returns predictions which is an ungrouped collection of source data. Maybe its not necesary to sort them?
-    #simple increment count of confusion matrix metric as it occurs?
-print helper_transform_sources()
+def transform_source(filename):
+    with open(filename) as jsonfile:
+        return {id: {'id': id,
+                     'source': value['source'],
+                     'is_closed': value['source_label_is_closed'] == 'Y'}
+                for id, value in json.load(jsonfile).items()}
 
-'''
-def helper_metrics()
-    metrics = []
-    #metric {"TP": 0, "FP": 0, "FN": 0, "TN": 0, "accuracy": , ...}
+def get_metrics(researcher_data, source_predictions):
+    metrics = {}
+    for company_id, prediction in source_predictions.items():
+        source = prediction['source']
+        is_closed = prediction['is_closed']
 
-    #keys common to both dictionaries
-    common_keys = set(helper_transform_sources.keys()).intersection(set(helper_combine_researchers.keys()))
+        if source not in metrics:
+            metrics[source] = {'TP': 0, 'FP': 0, 'TN': 0, 'FN': 0}
 
-    for company_id in common_keys:
-        #create a matrix dict for company if one does not exist 
-        # ...
-        #else increment value or key
-        metric["Accuracy"] = float(metric["TP"] + metric["TN"]) / metric["TP"] + metric["FN"] + metric["FP"] + metric["TN"] )
-        metric["True_positive_rate"] = float(metric["TP"]) / (metric["TP"] + metric["FN"])
-        metric["True_negative_rate"] = float(metric["TN"]) / (metric["FP"] + metric["TN"])
-        metric["Positive_predictive_value"] = float(metric["TP"]) / (metric["TP"] + metric["FP"])
-        metric["Negative_predictive_value"] = float(metric["TN"]) / (metric["TN"] + metric["FN"])
-        metric["False_positive_rate"] = float(metric["FP"]) / (metric["FP"] + metric["TN"])
-        # TP
-        if (helper_combine_researchers()[company_id]["is_closed"] == True and helper_transform_sources()[company_id]["is_closed"] == True):
-            metric["TP"] += 1
-        #FP
-        elif (helper_combine_researchers()[company_id]["is_closed"] == False and helper_transform_sources()[company_id]["is_closed"] == True):
-            metric["FP"] += 1
-        #FN
-        elif (helper_combine_researchers()[company_id]["is_closed"] == True and helper_transform_sources()[company_id]["is_closed"] == False):
-        #TN
-            metric["FN"] += 1
+        if (researcher_data[company_id]["is_closed"] and is_closed):
+            # i want hash to have the name of company_id =>'RHSJEAKEF'
+            metrics[source]["TP"] += 1
+        elif (not researcher_data[company_id]["is_closed"] and is_closed):
+            metrics[source]["FP"] += 1
+        elif (researcher_data[company_id]["is_closed"] and not is_closed):
+            metrics[source]["FN"] += 1
         else:
-            metric["TN"] += 1
+            metrics[source]["TN"] += 1
     return metrics
 
+#how do I get these working now?
+def accuracy(matrix):
+    matrix = get_metrics(researcher_data, source_predictions)
+    return float(matrix["TP"] + matrix["TN"]) / sum(matrix.values())
 
-def main('path to parent?')
-    #helper_transform_researchers_data('everything in data') 
-    #helper_transform_sources('everything in sources') 
-       
-       #=>>> outputs metrics to Analysis.csv
+def true_positive_rate(matrix):
+    return float(matrix["TP"]) / (matrix["TP"] + matrix["FN"])
 
-pass
+def true_negative_rate(matrix):
+    return float(matrix["TN"]) / (matrix["FP"] + matrix["TN"])
 
-#main('some path')
-'''
+def positive_predictive_value(matrix):
+    return float(matrix["TP"]) / (matrix["TP"] + matrix["FP"])
+
+def negative_predictive_value(matrix):
+    return float(matrix["TN"]) / (matrix["TN"] + matrix["FN"])
+
+def false_positive_rate(matrix):
+    return float(matrix["FP"]) / (matrix["FP"] + matrix["TN"])
+
+def main():
+    researcher_data = {}
+    for f in os.listdir(os.path.join('data')):
+        researcher_data.update(transform_researcher_data(os.path.join('data', f)))
+
+    source_predictions = {}
+    for f in os.listdir(os.path.join('sources')):
+        source_predictions.update(transform_source(os.path.join('sources', f)))
+
+   #for item in get_metrics, write to csv, and othe functions
+    matrix = get_metrics(researcher_data, source_predictions)#=> {{}, {}}
+    #the other metrics
+    accuracy(get_metrics(researcher_data, source_predictions))
+    true_positive_rate(matrix)
+    true_negative_rate(matrix)
+    positive_predictive_value(matrix)
+    negative_predictive_value(matrix)
+    false_positive_rate(matrix)
+
+    # writer = csv.writer(open('analysis.csv', 'wb'))
+    #for key, value in get_metrics.items():
+     #   writer.writerow([key, value])
+     #csv.writer(close())
+    return
+
+if __name__ == '__main__':
+    main()
